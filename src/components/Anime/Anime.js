1808231fe1176f78532463e536cf46c9a0de6fec
coderyeasin <yeasin3233@gmail.com>
2022-04-03 03:14:19 +0600

    data load

--- src/components/Anime/Anime.js
+++ src/components/Anime/Anime.js
@@ -1,11 +1,35 @@
-import { Button } from '@mui/material';
-import React from 'react';
+import { Button, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 
 const Anime = () => {
+
+    const [anime, setAnime] = useState([])
+    useEffect(() => {
+        fetch("https://ghibliapi.herokuapp.com/films")
+            .then((res) => res.json())
+            .then((data) => {
+            setAnime(data)
+        })
+    },[])
+
+
     return (
       <div>
         <h3>This is anime</h3>
         <Button variant="contained">Hello World</Button>
+            {anime.map((e) => {
+                return (
+                  <div key={e.id}>
+                    <Typography
+                      variant="subtitle1"
+                      gutterBottom
+                      component="div"
+                    >
+                      {e.title}
+                    </Typography>
+                  </div>
+                );
+       })}
       </div>
     );
 };
