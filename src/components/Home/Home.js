514183ceced4c34c4c207f0145431c00e29d873b
coderyeasin <yeasin3233@gmail.com>
2022-04-03 15:43:24 +0600

    navbar add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Anime from '../Anime/Anime';
+import Navbar from './Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
+            <Navbar />
             <Anime />
         </div>
     );
