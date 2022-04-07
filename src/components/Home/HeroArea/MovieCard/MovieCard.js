277952b39e5cc00f573d12c20516eecd7a0afe35
coderyeasin <yeasin3233@gmail.com>
2022-04-08 03:23:12 +0600

    card

--- src/components/Home/HeroArea/MovieCard/MovieCard.js
+++ src/components/Home/HeroArea/MovieCard/MovieCard.js
@@ -5,31 +5,50 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { Box } from '@mui/material';
 
 
 const MovieCard = (props) => {
     const{title, image, description} = props.movie
     return (
-      <Card sx={{ position: "relative", maxHeight:"500px" }}>
-        <CardMedia component="img" image={image} alt="green iguana" />
-        <CardContent sx={{ position: "absolute", top:"0" }}>
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            style={{ color: "pink", fontSize: "24px" }}
-          >
-            {title}
-          </Typography>
-          <Typography variant="body2" color="red">
-            {description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
+      <Box sx={{ position: "relative", maxHeight: "500px" }}>
+        <Card>
+          <CardMedia component="img" image={image} alt="green iguana" />
+          <CardContent sx={{ position: "absolute", top: "0" }}>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              style={{
+                color: "#05595B",
+                fontWeight: "bold",
+                fontSize: "24px",
+                background: "#E2D784",
+                width: "100px",
+                textAlign: "center",
+                borderRadius: "25px",
+              }}
+            >
+              New
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              style={{ color: "pink", fontSize: "24px" }}
+            >
+              {title}
+            </Typography>
+            <Typography variant="body2" color="red">
+              {description}
+            </Typography>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+      </Box>
     );
 };
 
